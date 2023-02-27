70ba44e166c2fc5c187cb619279c2b8106f10440
Adhara Redruello <113715775+adharainfinita@users.noreply.github.com>
2023-02-27 19:55:13 -0300

    Add files via upload

---
+++ Curso-js-openBootCamp/tarea.js
@@ -0,0 +1,12 @@
+var me = {
+    name: "Adhara",
+    age: 25,
+    developement: false,
+    birthday: 1997,
+    favoriteBook: malaz ={
+        title: "malaz, el libro de los caídos",
+        autor: "Steven Erikson",
+        date: 2010,
+        url: null
+    }
+}
